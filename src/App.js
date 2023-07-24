f2ad03854a81c64f265e54b4a6e6c7e44925b9ba
Saurabh Kumar <akgec.saurabh@gmail.com>
2023-07-24 06:44:34 +0530

    slider added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
+import Slider from "./components/Slider/Slider";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Slider />
       <Footer />
     </div>
   );
